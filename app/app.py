33c6135efe5d58e0ac952a3c464d03c40a3e7169
Marek Vasko <xvasko16@vutbr.cz>
2022-03-15 18:33:17 +0100

    Added fixes to app

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from pip import main
 
-
+from datetime import datetime
 import time
 
 if __name__ == '__main__':
     while True:
-        print("Hello @ " + str(time.now()))
+        print("Hello @ " + str(datetime.now()))
         time.sleep(5)
