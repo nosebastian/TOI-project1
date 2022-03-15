a4d71a9fb9c3b3e4f2128b6420b17a1b8c82e572
Marek Vasko <xvasko16@vutbr.cz>
2022-03-15 17:43:54 +0100

    Updated setup sctripts

--- rpiscripts/rpisetup.sh
+++ rpiscripts/rpisetup.sh
@@ -4,6 +4,7 @@ if ping -c 3 8.8.8.8
 then
     if [ $1 == 1 ]; then
         sudo apt update
+        sudo apt install git -y
         sudo apt upgrade -y
         sudo apt-get remove docker docker-engine docker.io containerd runc
         sudo apt-get install \
@@ -20,30 +21,28 @@ then
 
         sudo systemctl enable docker.service
         sudo systemctl enable containerd.service
-
-        sudo systemctl reboot
-    elif [ $1 == 2 ]; then
         
         sudo usermod -aG docker $USER
 
-        newgrp docker
-
-        docker version
-        
         DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
         mkdir -p $DOCKER_CONFIG/cli-plugins
         curl -SL "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-aarch64" -o $DOCKER_CONFIG/cli-plugins/docker-compose
         chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
+        sudo systemctl reboot
+    elif [ $1 == 2 ]; then
+        echo "DOCKER CHECK"
+        docker version
+        echo "DOCKER COMPOSE CHECK"
         docker compose version
-
-        sudo apt install git -y
+        echo "SETUP REPOSITORY"
         git clone https://github.com/marekkles/TOI-project1.git
+        #TODO AUTO SETUP
     else
         echo "Unknown option use:\n" >&2
         echo "./rpisetup 1|2" >&2
-        echo "  1 - install docker and reboot" >&2
-        echo "  2 - install docker compose and setup environment" >&2
+        echo "  1 - install docker, docker compose and reboot" >&2
+        echo "  2 - setup environment" >&2
     fi
 else
     echo "Connection is not established" >&2
