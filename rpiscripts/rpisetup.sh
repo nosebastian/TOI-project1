3e3422998bb075359126ad9f385f3beff46f948d
Marek Vasko <xvasko16@vutbr.cz>
2022-03-22 08:27:56 +0100

    Added multiarchitecture support

--- rpiscripts/rpisetup.sh
+++ rpiscripts/rpisetup.sh
@@ -13,8 +13,9 @@ then
             gnupg \
             lsb-release -y
         curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+        arch=`dpkg --print-architecture`
         echo \
-            "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+            "deb [arch=$arch signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
             $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
         sudo apt-get update
         sudo apt-get install containerd.io docker-ce docker-ce-cli -y || echo "Failed but continue"
@@ -24,9 +25,24 @@ then
         
         sudo usermod -aG docker $USER
 
+        if [ $arch == 'arm64' ]; then
+            compose_arch='aarch64'
+        elif [ $arch == 'armhf' ]; then
+            compose_arch='armv7'
+        elif [ $arch == 'armel' ]; then
+            compose_arch='armv6'
+        elif [ $arch == 'amd64' ]; then
+            compose_arch='x86_64'
+        elif [ $arch == 'i386' ]; then
+            compose_arch='x86_64'
+        else
+            echo "Unknown architecture" >&2
+            exit 4
+        fi
+
         DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
         mkdir -p $DOCKER_CONFIG/cli-plugins
-        curl -SL "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-aarch64" -o $DOCKER_CONFIG/cli-plugins/docker-compose
+        curl -SL "https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-$compose_arch" -o $DOCKER_CONFIG/cli-plugins/docker-compose
         chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
 
         sudo systemctl reboot
