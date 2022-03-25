7c03ea16d4f0d6ea31e1b81c614987145bd25f70
Lukas <erlich.lukas@gmail.com>
2022-03-25 12:34:07 +0100

    ESP32 Firmware update - reading analog and temp sensor

--- src/main.c
+++ src/main.c
@@ -1,18 +1,96 @@
+/* TODO ---------------
+  * ESP-NOW P2P
+  * ADC config
+  * convert raw ADC to lumens???
+  * WiFi RPi communication
+-----------------------*/
+
+/* NOTES --------------
+  * 4k7 resistor (DS18B20 & photoresistor)
+  * PR on pin 34 (ADC1)
+  * DS on pin 4
+-----------------------*/
+
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
 #include "esp_system.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 
+#include "owb.h"
+#include "owb_rmt.h"
+#include "ds18b20.h"
 
 #include "driver/gpio.h"
 
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
 
-#define GPIO_LED_RED  2
-#define GPIO_LED1     13
-#define GPIO_LED2     12
-#define GPIO_LED3     14
+#define GPIO_DS18B20_0      4
+#define MAX_DEVICES         8
+#define DS18B20_RESOLUTION  DS18B20_RESOLUTION_12_BIT
+#define SAMPLE_PERIOD       1000   // milliseconds
+#define GPIO_PHOTORESISTOR  2
 
+// ADC vars and consts
+#define DEFAULT_VREF    1100        //Use adc2_vref_to_gpio() to obtain a better estimate
+#define NO_OF_SAMPLES   64          //Multisampling
+
+static esp_adc_cal_characteristics_t *adc_chars;
+static const adc_channel_t channel = ADC_CHANNEL_6;     //GPIO34 if ADC1, GPIO14 if ADC2
+static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+static const adc_atten_t atten = ADC_ATTEN_DB_6;      // ADC_ATTEN_DB_6, ADC_ATTEN_DB_11
+static const adc_unit_t unit = ADC_UNIT_1;
+
+static void check_efuse(void)
+{
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_TP) == ESP_OK) {
+        printf("eFuse Two Point: Supported\n");
+    } else {
+        printf("eFuse Two Point: NOT supported\n");
+    }
+    //Check Vref is burned into eFuse
+    if (esp_adc_cal_check_efuse(ESP_ADC_CAL_VAL_EFUSE_VREF) == ESP_OK) {
+        printf("eFuse Vref: Supported\n");
+    } else {
+        printf("eFuse Vref: NOT supported\n");
+    }
+}
+
+static void print_char_val_type(esp_adc_cal_value_t val_type)
+{
+    if (val_type == ESP_ADC_CAL_VAL_EFUSE_TP) {
+        printf("Characterized using Two Point Value\n");
+    } else if (val_type == ESP_ADC_CAL_VAL_EFUSE_VREF) {
+        printf("Characterized using eFuse Vref\n");
+    } else {
+        printf("Characterized using Default Vref\n");
+    }
+}
+
+void light_intensity_task(void *pvParameter)
+{
+    while (1) {
+        uint32_t adc_reading = 0;
+        //Multisampling
+        for (int i = 0; i < NO_OF_SAMPLES; i++) {
+            if (unit == ADC_UNIT_1) {
+                adc_reading += adc1_get_raw((adc1_channel_t)channel);
+            } else {
+                int raw;
+                adc2_get_raw((adc2_channel_t)channel, width, &raw);
+                adc_reading += raw;
+            }
+        }
+        adc_reading /= NO_OF_SAMPLES;
+        //Convert adc_reading to voltage in mV
+        uint32_t voltage = esp_adc_cal_raw_to_voltage(adc_reading, adc_chars);
+        printf("Raw: %d\tVoltage: %dmV\n", adc_reading, voltage);
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 void hello_task(void *pvParameter)
 {
@@ -23,33 +101,104 @@ void hello_task(void *pvParameter)
     }
 }
 
-void blink_task(void *pvParameter) {
-    gpio_pad_select_gpio(GPIO_LED_RED);
-    gpio_pad_select_gpio(GPIO_LED1);
-    gpio_pad_select_gpio(GPIO_LED2);
-    gpio_pad_select_gpio(GPIO_LED3);
+void onewire_task(void *pvParameter)
+{
+    // Stable readings require a brief period before communication
+    vTaskDelay(2000.0 / portTICK_PERIOD_MS);
+
+    // Create a 1-Wire bus, using the RMT timeslot driver
+    OneWireBus * owb;
+    owb_rmt_driver_info rmt_driver_info;
+    owb = owb_rmt_initialize(&rmt_driver_info, GPIO_DS18B20_0,
+                             RMT_CHANNEL_1, RMT_CHANNEL_0);
+    owb_use_crc(owb, true);  // enable CRC check for ROM code
+
+    // Find all connected devices
+    ESP_LOGI("one-wire", "Find devices:\n");
+    OneWireBus_ROMCode device_rom_codes[MAX_DEVICES] = {0};
+    int num_devices = 0;
+    OneWireBus_SearchState search_state = {0};
+    bool found = false;
+    owb_search_first(owb, &search_state, &found);
+    while (found)
+    {
+        char rom_code_s[17];
+        owb_string_from_rom_code(search_state.rom_code, rom_code_s,
+                                 sizeof(rom_code_s));
+        printf("  %d : %s\n", num_devices, rom_code_s);
+        device_rom_codes[num_devices] = search_state.rom_code;
+        ++num_devices;
+        owb_search_next(owb, &search_state, &found);
+    }
+    ESP_LOGI("one-wire", "Found %d device%s", num_devices, 
+                         num_devices == 1 ? "" : "s");
 
 
-    /* Set the GPIO as a push/pull output */
-    ESP_ERROR_CHECK(gpio_set_direction(GPIO_LED_RED, GPIO_MODE_OUTPUT));
-    ESP_ERROR_CHECK(gpio_set_direction(GPIO_LED1, GPIO_MODE_OUTPUT));
-    ESP_ERROR_CHECK(gpio_set_direction(GPIO_LED2, GPIO_MODE_OUTPUT));
-    ESP_ERROR_CHECK(gpio_set_direction(GPIO_LED3, GPIO_MODE_OUTPUT));
 
-    int cnt = 0;
+    // Create DS18B20 devices on the 1-Wire bus
+    DS18B20_Info * devices[MAX_DEVICES] = {0};
+    for (int i = 0; i < num_devices; ++i)
+    {
+        DS18B20_Info * ds18b20_info = ds18b20_malloc();  
+        // heap allocation
+        devices[i] = ds18b20_info;
 
-    while(1) {
-        /* Blink off (output low) */
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_LED_RED, cnt & 0x01));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_LED1, cnt & 0x01));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_LED2, cnt & 0x02));
-        ESP_ERROR_CHECK(gpio_set_level(GPIO_LED3, cnt & 0x04));
-        vTaskDelay(500 / portTICK_RATE_MS);
-        cnt++;
+        if (num_devices == 1)
+        {
+            ESP_LOGI("one-wire", "Single device optimisations enabled");
+            ds18b20_init_solo(ds18b20_info, owb);          
+            // only one device on bus
+        }
+        else
+        {
+            ds18b20_init(ds18b20_info, owb, device_rom_codes[i]); 
+            // associate with bus and device
+        }
+        ds18b20_use_crc(ds18b20_info, true);           
+        // enable CRC check on all reads
+        ds18b20_set_resolution(ds18b20_info, DS18B20_RESOLUTION);
+    }
+
+    // Read temperatures from all sensors sequentially
+    while (1)
+    {
+        ESP_LOGI("one-wire", "Temperature readings (degrees C):");
+        ds18b20_convert_all(owb);
+
+        // In this application all devices use the same resolution,
+        // so use the first device to determine the delay
+        ds18b20_wait_for_conversion(devices[0]);
+        
+        for (int i = 0; i < num_devices; ++i)
+        {
+            float temp = 0;
+            ds18b20_read_temp(devices[i], &temp);
+            ESP_LOGI("one-wire", "  %d: %.3f\n", i, temp);
+        }
+        vTaskDelay(1000 / portTICK_PERIOD_MS); 
     }
 }
+
 void app_main() {
+
+    // check ADC
+    check_efuse();
+
+    //Configure ADC
+    if (unit == ADC_UNIT_1) {
+        adc1_config_width(width);
+        adc1_config_channel_atten(channel, atten);
+    } else {
+        adc2_config_channel_atten((adc2_channel_t)channel, atten);
+    }
+
+    //Characterize ADC
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(unit, atten, width, DEFAULT_VREF, adc_chars);
+    print_char_val_type(val_type);
+
     ESP_ERROR_CHECK(nvs_flash_init());
-    xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
-    xTaskCreate(&blink_task, "blink_task", 512, NULL, 5, NULL);
+    //xTaskCreate(&hello_task, "hello_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&onewire_task, "one_wire_task", 2048, NULL, 5, NULL);
+    xTaskCreate(&light_intensity_task, "light_intensity_task", 2048, NULL, 5, NULL);
 }
